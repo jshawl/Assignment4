712b072b5258bbbfff8ed1e4c158f53eb6096b27
Jesse Shawl <jesse@jshawl.com>
2015-04-13 11:53:58 -0400

    present view controller

--- Lesson04/ArrayViewController.swift
+++ Lesson04/ArrayViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ArrayViewController: UIViewController {
-
+class ArrayViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableData = ["one", "two"]
+    let cellIdentifier = "arrayCell"
     override func viewDidLoad() {
         super.viewDidLoad()
 //        self.dismissViewControllerAnimated(false, completion: nil)
@@ -17,11 +20,37 @@ class ArrayViewController: UIViewController {
             self.view.backgroundColor = UIColor.blueColor()
         })
         self.view.backgroundColor = UIColor.redColor()
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+
         /*
         TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
         TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
         
         */
     }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return tableData.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        // Configure the cell...
+        cell.textLabel?.text = tableData[indexPath.row]
+        return cell
+    }
+    
+
 }
 
